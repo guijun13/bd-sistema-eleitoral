1f0a44f7751a39b7b5de5bd287f693759ac6aadb
samuks123 <103591276+samuks123@users.noreply.github.com>
2024-05-17 12:07:26 -0300

    Add files via upload

---
+++ sql/Trab_DDL.sql
@@ -0,0 +1,120 @@
+CREATE TABLE individuo (
+    cpf CHAR(11),
+    nome VARCHAR(255) NOT NULL,
+    tipo VARCHAR(50),
+    rg CHAR(16),
+    data_de_nascimento DATE,
+    nacionalidade VARCHAR(100),
+    titulo_eleitor CHAR(12),
+
+	CONSTRAINT individuo_pk PRIMARY KEY (cpf),
+	CONSTRAINT individuo_sk unique (titulo_eleitor)
+);
+
+CREATE TABLE processo_judicial (
+	nro_processo CHAR(25),
+	data DATE,
+	acusado CHAR(11),
+
+	CONSTRAINT processo_judicial_pk PRIMARY KEY (nro_processo),
+	CONSTRAINT processo_judicial_fk FOREIGN KEY (acusado) REFERENCES individuo(cpf)
+);
+
+CREATE TABLE partido (
+	id_partido SERIAL,
+	nome VARCHAR(255) NOT NULL,
+	abreviação VARCHAR(16),
+	nro_integrantes NUMERIC,
+
+	CONSTRAINT partido_pk PRIMARY KEY (id_partido)
+);
+
+CREATE TABLE programa_partido (
+	id_programa SERIAL,
+	partido SERIAL,
+	descrição TEXT,
+
+	CONSTRAINT programa_partido_pk PRIMARY KEY (id_programa, partido),
+	CONSTRAINT programa_partido_fk FOREIGN KEY (partido) REFERENCES partido(id_partido)
+);
+
+CREATE TABLE candidato (
+	cpf CHAR(11),
+	vice BOOLEAN,
+	partido SERIAL,
+
+	CONSTRAINT candidato_pf PRIMARY KEY (cpf),
+	CONSTRAINT candidato_fk_1 FOREIGN KEY (cpf) REFERENCES individuo(cpf),
+	CONSTRAINT candidato_fk_2 FOREIGN KEY (partido) REFERENCES partido(id_partido)
+);
+
+CREATE TABLE pleito (
+	nome VARCHAR(255),
+	quantidade_votos NUMERIC,
+
+	CONSTRAINT pleito_pk PRIMARY KEY (nome)
+);
+
+CREATE TABLE cargo (
+	id_cargo SERIAL,
+	titulo VARCHAR(255),
+	tipo VARCHAR(9),
+
+	CONSTRAINT cargo_pk PRIMARY KEY (id_cargo),
+	CONSTRAINT cargo_tipo_ck check (tipo in ('Municipal', 'Estadual', 'Federal'))
+);
+
+CREATE TABLE equipe_de_apoio (
+	id_equipe SERIAL,
+	nome VARCHAR(255),
+	nro_membros NUMERIC,
+
+	CONSTRAINT equipe_de_apoio_pk PRIMARY KEY (id_equipe)
+);
+
+CREATE TABLE candidatura (
+	id_candidatura SERIAL,
+	candidato_principal CHAR(11),
+	candidato_vice CHAR(11),
+	cargo SERIAL,
+	equipe_de_apoio SERIAL,
+	pleito VARCHAR(255),
+	ano NUMERIC,
+
+	CONSTRAINT candidatura_pk PRIMARY KEY (id_candidatura),
+	CONSTRAINT candidatuta_fk_1 FOREIGN KEY (candidato_principal) REFERENCES candidato(cpf),
+	CONSTRAINT candidatuta_fk_2 FOREIGN KEY (candidato_vice) REFERENCES candidato(cpf),
+	CONSTRAINT candidatuta_fk_3 FOREIGN KEY (cargo) REFERENCES cargo(id_cargo),
+	CONSTRAINT candidatuta_fk_4 FOREIGN KEY (equipe_de_apoio) REFERENCES equipe_de_apoio(id_equipe),
+	CONSTRAINT candidatuta_fk_5 FOREIGN KEY (pleito) REFERENCES pleito(nome)
+);
+
+CREATE TABLE doadores (
+	cpf CHAR(11),
+	valor_doação NUMERIC,
+	candidatura SERIAL,
+
+	CONSTRAINT doadores_pk PRIMARY KEY (cpf),
+	CONSTRAINT doadores_fk_1 FOREIGN KEY (cpf) REFERENCES individuo(cpf),
+	CONSTRAINT doadores_fk_2 FOREIGN KEY (candidatura) REFERENCES candidatura(id_candidatura)
+);
+
+CREATE TABLE empresa (
+	cnpj CHAR(14),
+	nome VARCHAR(255),
+	localização VARCHAR(255),
+	valor_doação NUMERIC,
+	candidatura SERIAL,
+
+	CONSTRAINT empresa_pk PRIMARY KEY (cnpj),
+	CONSTRAINT empresa_fk FOREIGN KEY (candidatura) REFERENCES candidatura(id_candidatura)
+);
+
+CREATE TABLE participantes_equipe_de_apoio (
+	cpf CHAR(11),
+	equipe_de_apoio SERIAL,
+
+	CONSTRAINT participantes_equipe_de_apoio_pk PRIMARY KEY (cpf),
+	CONSTRAINT participantes_equipe_de_apoio_fk_1 FOREIGN KEY (cpf) REFERENCES individuo(cpf),
+	CONSTRAINT participantes_equipe_de_apoio_fk_2 FOREIGN KEY (equipe_de_apoio) REFERENCES equipe_de_apoio(id_equipe)
+);
