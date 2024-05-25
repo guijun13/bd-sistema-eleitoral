842684e2893cd955cbbae4034bcfd6b0a830b457
guijun13 <guilhermejun13.grillo@gmail.com>
2024-05-25 16:43:15 -0300

    add more consultas especificas

--- sql/consultasEspecificas.sql
+++ sql/consultasEspecificas.sql
@@ -37,3 +37,38 @@ FROM candidatura c
   LEFT JOIN individuo i_vice ON candidato_vice.cpf = i_vice.cpf
   JOIN cargo ON c.cargo = cargo.id_cargo
 ORDER BY cargo.titulo;
+-- Relatório de candidaturas eleitas, incluindo candidatos principais e vices
+SELECT c.id_candidatura,
+  c.ano,
+  i_principal.nome AS candidato_principal,
+  i_vice.nome AS candidato_vice,
+  cargo.titulo AS cargo
+FROM candidatura c
+  JOIN candidato candidato_principal ON c.candidato_principal = candidato_principal.cpf
+  JOIN individuo i_principal ON candidato_principal.cpf = i_principal.cpf
+  LEFT JOIN candidato candidato_vice ON c.candidato_vice = candidato_vice.cpf
+  LEFT JOIN individuo i_vice ON candidato_vice.cpf = i_vice.cpf
+  JOIN cargo ON c.cargo = cargo.id_cargo
+ORDER BY c.ano,
+  cargo.titulo,
+  i_principal.nome;
+-- Consulta SQL para Listar Pessoas Ficha Limpa
+WITH processos_recente AS (
+  SELECT acusado
+  FROM processo_judicial
+  WHERE data >= (CURRENT_DATE - INTERVAL '5 YEARS')
+)
+SELECT i.cpf,
+  i.nome,
+  i.tipo,
+  i.rg,
+  i.data_de_nascimento,
+  i.nacionalidade,
+  i.titulo_eleitor
+FROM individuo i
+  LEFT JOIN (
+    SELECT acusado
+    FROM processo_judicial
+    WHERE data >= (CURRENT_DATE - INTERVAL '5 YEARS')
+  ) pr ON i.cpf = pr.acusado
+WHERE pr.acusado IS NULL;
