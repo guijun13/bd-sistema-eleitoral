0f15c4d67295e0b645cbba59abd5a885e811ebce
Lucas Issao Omati <lucasomatioficial@gmail.com>
2024-05-26 16:39:15 -0300

    Add 3 new triggers

--- sql/trigger.sql
+++ sql/trigger.sql
@@ -1,7 +1,7 @@
+-- Incrementa o número de membros na equipe correspondente
 CREATE OR REPLACE FUNCTION aumentar_membros_equipe_de_apoio()
 RETURNS TRIGGER AS $$
 BEGIN
-    -- Incrementa o número de membros na equipe correspondente
     UPDATE equipe_de_apoio
     SET nro_membros = nro_membros + 1
     WHERE id_equipe = NEW.equipe_de_apoio;
@@ -17,10 +17,10 @@ EXECUTE FUNCTION aumentar_membros_equipe_de_apoio();
 
 -- ////////////////////////////////////////////////////////////
 
+-- Decrementa o número de membros na equipe correspondente
 CREATE OR REPLACE FUNCTION diminuir_membros_equipe_de_apoio()
 RETURNS TRIGGER AS $$
 BEGIN
-    -- Decrementa o número de membros na equipe correspondente
     UPDATE equipe_de_apoio
     SET nro_membros = nro_membros - 1
     WHERE id_equipe = OLD.equipe_de_apoio;
@@ -36,15 +36,14 @@ EXECUTE FUNCTION diminuir_membros_equipe_de_apoio();
 
 -- ////////////////////////////////////////////////////////////
 
+-- Decrementa o número de membros na equipe antiga e incrementa na nova equipe
 CREATE OR REPLACE FUNCTION atualizar_membros_equipe_de_apoio()
 RETURNS TRIGGER AS $$
 BEGIN
-    -- Decrementa o número de membros na equipe antiga
     UPDATE equipe_de_apoio
     SET nro_membros = nro_membros - 1
     WHERE id_equipe = OLD.equipe_de_apoio;
-
-    -- Incrementa o número de membros na nova equipe
+    
     UPDATE equipe_de_apoio
     SET nro_membros = nro_membros + 1
     WHERE id_equipe = NEW.equipe_de_apoio;
@@ -61,15 +60,14 @@ EXECUTE FUNCTION atualizar_membros_equipe_de_apoio();
 
 -- ////////////////////////////////////////////////////////////
 
+-- Verifica se o CPF do novo candidato tem um processo judicial no periodo de 5 anos para realizar a cadastro da candidatura
 CREATE OR REPLACE FUNCTION verificar_ficha_limpa()
 RETURNS TRIGGER AS $$
 DECLARE
     data_limite DATE;
 BEGIN
-    -- Definindo a data limite como 5 anos atrás a partir da data atual
     data_limite := CURRENT_DATE - INTERVAL '5 years';
 
-    -- Verifica se o CPF do novo candidato tem um processo judicial recente
     IF EXISTS (
         SELECT 1
         FROM processo_judicial
@@ -87,3 +85,65 @@ CREATE TRIGGER trigger_verificar_ficha_limpa
 BEFORE INSERT ON candidato
 FOR EACH ROW
 EXECUTE FUNCTION verificar_ficha_limpa();
+
+-- ////////////////////////////////////////////////////////////
+
+-- Incrementa o número de integrantes do novo partido no insert da candidatura
+CREATE OR REPLACE FUNCTION insert_nro_integrantes()
+RETURNS TRIGGER AS $$
+BEGIN
+    UPDATE partido
+    SET nro_integrantes = nro_integrantes + 1
+    WHERE id_partido = NEW.partido;
+
+    RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_insert_nro_integrantes
+AFTER INSERT ON candidato
+FOR EACH ROW
+EXECUTE FUNCTION insert_nro_integrantes();
+
+-- ////////////////////////////////////////////////////////////
+
+-- Incrementa o número de integrantes do novo partido e decrementa o do antigo
+CREATE OR REPLACE FUNCTION update_nro_integrantes()
+RETURNS TRIGGER AS $$
+BEGIN
+    UPDATE partido
+    SET nro_integrantes = nro_integrantes + 1
+    WHERE id_partido = NEW.partido;
+
+    UPDATE partido
+    SET nro_integrantes = nro_integrantes - 1
+    WHERE id_partido = OLD.partido;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_update_nro_integrantes
+AFTER UPDATE ON candidato
+FOR EACH ROW
+WHEN (OLD.partido IS DISTINCT FROM NEW.partido)
+EXECUTE FUNCTION update_nro_integrantes();
+
+-- ////////////////////////////////////////////////////////////
+
+-- Decrementa o número de integrantes do partido correspondente ao delete na candidatura
+CREATE OR REPLACE FUNCTION delete_nro_integrantes()
+RETURNS TRIGGER AS $$
+BEGIN
+    UPDATE partido
+    SET nro_integrantes = nro_integrantes - 1
+    WHERE id_partido = OLD.partido;
+
+    RETURN OLD;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_delete_nro_integrantes
+AFTER DELETE ON candidato
+FOR EACH ROW
+EXECUTE FUNCTION delete_nro_integrantes();
