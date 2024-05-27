a3c68a5f583e75f417cefba0ea335ff81a033de9
guijun13 <guilhermejun13.grillo@gmail.com>
2024-05-26 22:43:09 -0300

    update query eleitos

--- app/sql/consultasEspecificas.sql
+++ app/sql/consultasEspecificas.sql
@@ -38,20 +38,20 @@ FROM candidatura c
   JOIN cargo ON c.cargo = cargo.id_cargo
 ORDER BY cargo.titulo;
 -- Relatório de candidaturas eleitas, incluindo candidatos principais e vices
-SELECT c.id_candidatura,
-  c.ano,
-  i_principal.nome AS candidato_principal,
-  i_vice.nome AS candidato_vice,
-  cargo.titulo AS cargo
+SELECT c.ano,
+  i1.nome AS candidato_eleito,
+  i2.nome AS vice_eleito,
+  c.nro_votos
 FROM candidatura c
-  JOIN candidato candidato_principal ON c.candidato_principal = candidato_principal.cpf
-  JOIN individuo i_principal ON candidato_principal.cpf = i_principal.cpf
-  LEFT JOIN candidato candidato_vice ON c.candidato_vice = candidato_vice.cpf
-  LEFT JOIN individuo i_vice ON candidato_vice.cpf = i_vice.cpf
-  JOIN cargo ON c.cargo = cargo.id_cargo
-ORDER BY c.ano,
-  cargo.titulo,
-  i_principal.nome;
+  JOIN candidato cp ON c.candidato_principal = cp.cpf
+  JOIN individuo i1 ON cp.cpf = i1.cpf
+  LEFT JOIN candidato cv ON c.candidato_vice = cv.cpf
+  LEFT JOIN individuo i2 ON cv.cpf = i2.cpf
+WHERE c.nro_votos = (
+    SELECT MAX(c1.nro_votos)
+    FROM candidatura c1
+    WHERE c1.ano = c.ano
+  );
 -- Consulta SQL para Listar Pessoas Ficha Limpa
 WITH processos_recente AS (
   SELECT acusado
